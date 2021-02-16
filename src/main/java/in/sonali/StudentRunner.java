7e18d1b61811036ce65183d50edaece865e3f335
lenovo <lenovo@192.168.0.111>
2021-02-15 21:21:38 -1200

    WH-3452 Update Runner With Log4j Using SLF4j

--- src/main/java/in/sonali/StudentRunner.java
+++ src/main/java/in/sonali/StudentRunner.java
@@ -1,15 +1,23 @@
 package in.sonali;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
 @Component
 public class StudentRunner implements CommandLineRunner {
-
+	private static Logger log = LoggerFactory.getLogger(StudentRunner.class); 
 	@Override
 	public void run(String... args) throws Exception {
+		int a=10;
+		log.info("Stared");
 		// TODO Auto-generated method stub
-		System.out.println("WEL COME TO INDIA");
-		System.out.println("HELLO INDIA");
+		System.out.println("WEL COME TO INDIA"+a);
+		log.info("End");
+
 	}
 
 }
