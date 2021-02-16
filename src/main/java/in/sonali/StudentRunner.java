5a1d058f879b370cdfbb0d9c78ae20488fe6f619
lenovo <lenovo@192.168.0.111>
2021-02-15 20:32:34 -1200

    WH-9025 Next Commit

--- src/main/java/in/sonali/StudentRunner.java
+++ src/main/java/in/sonali/StudentRunner.java
@@ -9,6 +9,7 @@ public class StudentRunner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("WEL COME TO INDIA");
+		System.out.println("HELLO INDIA");
 	}
 
 }
---
+++ src/main/java/in/sonali/model/Student.java
@@ -0,0 +1,5 @@
+package in.sonali.model;
+
+public class Student {
+
+}
